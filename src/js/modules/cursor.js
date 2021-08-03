0f5aa7255571ac0ddefee1d2e7beb58291be312f
Hakan Bilgin <hbi99@hotmail.com>
2021-08-03 22:06:46 +0200

    cursor selection box

--- src/js/modules/cursor.js
+++ src/js/modules/cursor.js
@@ -15,9 +15,10 @@ const Cursor = {
 	},
 	dispatch(event) {
 		let APP = eniac,
+			Tools = APP.tools,
 			Self = Cursor,
-			sheet,
-			anchor,
+			sheet, table,
+			anchor, offset,
 			xNum, yNum,
 			el;
 		switch (event.type) {
@@ -34,15 +35,17 @@ const Cursor = {
 				// focus clicked table
 				Self.dispatch({ type: "focus-table", sheet });
 				// make column + row active
-				[yNum, xNum] = APP.tools.sheet.grid.getCoord(anchor[0]);
-				APP.tools.dispatch({ type: "select-coords", yNum, xNum });
+				[yNum, xNum] = Tools.sheet.grid.getCoord(anchor[0]);
+				Tools.dispatch({ type: "select-coords", yNum, xNum });
+				// UI select element
+				Self.dispatch({ ...event, anchor, type: "select-cell" });
 				break;
 			case "focus-table":
 				sheet = event.sheet;
-				if (APP.tools.sheet && sheet.isSame(APP.tools.sheet.el)) return;
+				if (Tools.sheet && sheet.isSame(Tools.sheet.el)) return;
 
 				// sync tools sheet
-				APP.tools.dispatch({ type: "sync-sheet-table", sheet });
+				Tools.dispatch({ type: "sync-sheet-table", sheet });
 				// show tools for table
 				Self.els.tools.removeClass("hidden");
 				// update sidebar
@@ -53,10 +56,26 @@ const Cursor = {
 					// update sidebar
 					APP.sidebar.dispatch({ type: "show-sheet" });
 					// hide table tools
-					APP.tools.dispatch({ type: "reset-tools" });
+					Tools.dispatch({ type: "reset-tools" });
 				}
 				break;
+			case "select-cell":
+				anchor = event.anchor;
+				table = anchor.parents(".tbl-root:first");
+
+				offset = Self.getOffset(anchor[0], table[0]);
+				Self.els.root.addClass("show").css(offset);
+				break;
 		}
+	},
+	getOffset(el, pEl) {
+		let rect1 = el.getBoundingClientRect(),
+			rect2 = pEl.getBoundingClientRect(),
+			top = Math.floor(rect1.top - rect2.top) + pEl.offsetTop - 2,
+			left = Math.floor(rect1.left - rect2.left) + pEl.offsetLeft - 2,
+			width = rect1.width + 5,
+			height = rect1.height + 5;
+		return { top, left, width, height };
 	},
 	resize(event) {
 		let APP = eniac,
