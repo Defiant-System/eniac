ea2694dd88bfb50b433c3a50739049d65730d570
Hakan Bilgin <hbi99@hotmail.com>
2021-09-19 21:52:28 +0200

    smart guides; added center points

--- src/js/modules/guides.js
+++ src/js/modules/guides.js
@@ -20,16 +20,22 @@ Smart.prototype = {
 					y = parseInt(el.css("top"), 10),
 					x = parseInt(el.css("left"), 10),
 					w = parseInt(el.css("width"), 10),
-					h = parseInt(el.css("height"), 10);
+					h = parseInt(el.css("height"), 10),
+					mh = h * .5,
+					mw = w * .5;
 				if (svg !== offset.el) {
-					found.push({ y, x, w, h });
+					found.push({ y, x, w, h, mh, mw });
 				}
 			});
 
 		this.sensivity = 10;
 
 		// add guide line element to "this"
-		this.offset = offset;
+		this.offset = {
+			...offset,
+			mh: (offset.h * .5),
+			mw: (offset.w * .5),
+		};
 
 		// add guide line element to "this"
 		this.lines = {
@@ -48,7 +54,7 @@ Smart.prototype = {
 			vert = { top: -1, left: -1, width: 1 },
 			hori = { top: -1, left: -1, height: 1 },
 			diag = { top: -1, left: -1, width: 1 },
-			calcH = (g, y, aT=0) => {
+			calcH = (g, y, add = { t: 0 }) => {
 				let minX = m.left,
 					maxX = g.x,
 					w = g.w;
@@ -58,9 +64,9 @@ Smart.prototype = {
 					maxX = m.left;
 					w = o.w;
 				}
-				return { top: g.y+aT, left: minX, width: maxX-minX+w };
+				return { top: g.y+add.t, left: minX, width: maxX-minX+w };
 			},
-			calcV = (g, x, aL=0) => {
+			calcV = (g, x, add = { l: 0 }) => {
 				let minY = m.top,
 					maxY = g.y,
 					h = g.h;
@@ -70,32 +76,40 @@ Smart.prototype = {
 					maxY = m.top;
 					h = o.h;
 				}
-				return { left: g.x+aL, top: minY, height: maxY-minY+h };
+				return { left: g.x+add.l, top: minY, height: maxY-minY+h };
 			};
 
 		this.map(g => {
 			let dy = m.top - g.y,
 				dx = m.left - g.x,
+
 				ohy = dy + o.h,
 				ghy = dy - g.h,
 				ogh = ohy - g.h,
+				oym = ohy - g.mh - o.mh,
+
 				owx = dx + o.w,
 				gwx = dx - g.w,
-				ogw = owx - g.w;
-			
+				ogw = owx - g.w,
+				oxm = owx - g.mw - o.mw;
+
 			// vertical comparisons
 			switch (true) {
-				case (dy  < s && dy  > -s): hori = calcH(g, dy        ); break;
-				case (ohy < s && ohy > -s): hori = calcH(g, ohy       ); break;
-				case (ghy < s && ghy > -s): hori = calcH(g, ghy, g.h-1); break;
-				case (ogh < s && ogh > -s): hori = calcH(g, ogh, g.h-1); break;
+				// case (dy  < s && dy  > -s): hori = calcH(g, dy);                break;
+				// case (ohy < s && ohy > -s): hori = calcH(g, ohy);               break;
+				// case (ghy < s && ghy > -s): hori = calcH(g, ghy, { t: g.h-1 }); break;
+				// case (ogh < s && ogh > -s): hori = calcH(g, ogh, { t: g.h-1 }); break;
+
+				case (oym < s && oym > -s): hori = calcH(g, oym, { t: g.mh }); break;
 			}
 			// horizontal comparisons
 			switch (true) {
-				case (dx  < s && dx  > -s): vert = calcV(g, dx        ); break;
-				case (owx < s && owx > -s): vert = calcV(g, owx       ); break;
-				case (gwx < s && gwx > -s): vert = calcV(g, gwx, g.w-1); break;
-				case (ogw < s && ogw > -s): vert = calcV(g, ogw, g.w-1); break;
+				// case (dx  < s && dx  > -s): vert = calcV(g, dx);               break;
+				// case (owx < s && owx > -s): vert = calcV(g, owx);              break;
+				// case (gwx < s && gwx > -s): vert = calcV(g, gwx, { l: g.w-1}); break;
+				// case (ogw < s && ogw > -s): vert = calcV(g, ogw, { l: g.w-1}); break;
+
+				case (oxm < s && oxm > -s): vert = calcV(g, oxm, { l: g.mw }); break;
 			}
 		});
 
