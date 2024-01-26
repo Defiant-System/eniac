a6504d45692c58913005b95f5279251b308d1251
Hakan Bilgin <hbi99@hotmail.com>
2024-01-26 23:48:55 +0100

    themes tweaks

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -5,7 +5,7 @@ let Test = {
 
 		// temp
 		// setTimeout(() => Self.dispatch({ type: "tab.new", spawn: Spawn }), 300);
-		setTimeout(() => eniac.spawn.dispatch({ type: "toggle-sidebar", value: true, spawn }), 350);
+		setTimeout(() => eniac.spawn.dispatch({ type: "toggle-sidebar", value: true, spawn }), 500);
 		// setTimeout(() => Spawn.find(".xl-shape:nth(0)").trigger("mousedown").trigger("mouseup"), 150);
 		
 		// setTimeout(() => Spawn.find(".block-buttons .btn:nth(0)").trigger("click"), 300);
@@ -32,7 +32,7 @@ let Test = {
 			// 	el.trigger("click");
 			// }, 100);
 
-		}, 700);
+		}, 800);
 
 		// setTimeout(() => {
 		// 	let target = eniac.spawn.sidebar.shape.parent.els.el.find(".gradient-colors .point:nth(0)")[0];
